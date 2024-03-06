2416abe78da99d9c69ce59c803d012cf828d0f5f
Bouchard François <e.fbouchard@etu.cvm.qc.ca>
2024-03-06 15:09:26 -0500

    ajout frame construction et frame amelioration

--- vue.py
+++ vue.py
@@ -7,6 +7,7 @@ class Vue:
         self.modele = modele
         self.root = Tk()
         self.root.title("Tower Defense")
+        self.dict_amelioration = {"default": "***cout;***desc;****tour"}
         ##creation  des interfaces graphiques
         self.dict_interfaces = {}
         self.creer_interfaces()
@@ -23,6 +24,16 @@ class Vue:
         canvas_aire_jeu.pack()
         self.dict_interfaces.update({"c_jeu": canvas_aire_jeu})
 
+
+        bouton_construction = Button(canvas_aire_jeu, text="construction",
+                              font=("Arial", 14), fg="blue", bg="lightgray", padx=10, pady=5, command=self.test_toggle_construction)
+
+        bouton_amelioration = Button(canvas_aire_jeu, text="amelioration",
+                              font=("Arial", 14), fg="blue", bg="lightgray", padx=10, pady=5, command=self.test_toggle_amelioration)
+
+        bouton_construction.place(relx=0.6, rely=0.1, anchor="center", relheight=0.1, relwidth=0.1)
+        bouton_amelioration.place(relx=0.8, rely=0.1, anchor="center", relheight=0.1, relwidth=0.1)
+
     def creer_frame_menu(self):
         frame_menu = Frame(self.root, width=self.modele.unite_base * 32,
                            height=self.modele.unite_base * 5, bg="red")
@@ -31,7 +42,7 @@ class Vue:
         self.dict_interfaces.update({"f_menu": frame_menu})
         # self.creer_frame_vague()
         self.creer_frame_construction()
-        # self.creer_frame_amelioration()
+        self.creer_frame_amelioration()
         # self.creer_frame_ressource()
 
     def creer_frame_construction(self):
@@ -42,21 +53,60 @@ class Vue:
 
         frame_construction.place(x=6 * ub, y=5)
 
-        label_projectile = Button(frame_construction, text="tour à projectile",
+        bouton_projectile = Button(frame_construction, text="tour à projectile",
                                   font=("Arial", 14), fg="blue", bg="gray", padx=10, pady=5,
                                   wraplength=ub * 2, command=self.construire_tour_projectile)
-        label_eclair = Button(frame_construction, text="tour à éclair",
+        bouton_eclair = Button(frame_construction, text="tour à éclair",
                               font=("Arial", 14), fg="blue", bg="lightgray", padx=10, pady=5,
                               wraplength=ub * 2, command=self.construire_tour_eclair)
-        label_poison = Button(frame_construction, text="tour de poison",
+        bouton_poison = Button(frame_construction, text="tour de poison",
                               font=("Arial", 14), fg="blue", bg="gray", padx=10, pady=5,
                               wraplength=ub * 2, command=self.construire_tour_poison)
 
-        label_projectile.place(relx=0.2, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
-        label_eclair.place(relx=0.5, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
-        label_poison.place(relx=0.8, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+        bouton_projectile.place(relx=0.2, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+        bouton_eclair.place(relx=0.5, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+        bouton_poison.place(relx=0.8, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
 
         self.dict_interfaces.update({"f_construction": frame_construction})
+        pass
+
+    def creer_frame_amelioration(self):
+        ub = self.modele.unite_base
+        cout, desc, tour = self.split_string_amelioration(self.dict_amelioration["default"])
+        frame_amelioration = Frame(self.dict_interfaces["f_menu"], width=ub * 12,
+                                   height=ub * 4, bg="purple")
+
+        self.dict_interfaces.update({"f_amelioration": frame_amelioration})
+
+        canvas_upgrade = Canvas(frame_amelioration, width=ub * 2,
+                                height=ub * 2, bg="blue")
+
+        canvas_upgrade.place(relx=0.2, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+
+        label_cout = Label(canvas_upgrade, text=cout,
+                           font=("Arial", 14), fg="blue", bg="teal", padx=10, pady=5,
+                           wraplength=ub * 2)
+        label_desc = Label(canvas_upgrade, text=desc,
+                           font=("Arial", 14), fg="blue", bg="red", padx=10, pady=5,
+                           wraplength=ub * 2)
+
+        label_cout.place(relx=0, rely=0, anchor="nw", relheight=0.3, relwidth=1)
+        label_desc.place(relx=0, rely=0.3, anchor="nw", relheight=0.7, relwidth=1)
+
+        bouton_upgrade = Button(frame_amelioration, text="Upgrade",
+                               font=("Arial", 14), fg="blue", bg="lightgray", padx=10, pady=5,
+                               wraplength=ub * 2, command=self.upgrade)
+        label_tour = Label(frame_amelioration, text=tour,
+                           font=("Arial", 14), fg="blue", bg="gray", padx=10, pady=5,
+                           wraplength=ub * 2)
+
+        bouton_upgrade.place(relx=0.5, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+        label_tour.place(relx=0.8, rely=0.5, anchor="center", relheight=0.5, relwidth=0.2)
+
+        self.dict_interfaces.update({"l_am_cout": label_cout})
+        self.dict_interfaces.update({"l_am_desc": label_desc})
+        self.dict_interfaces.update({"l_am_tour": label_tour})
+        # frame_amelioration.place(x=6 * ub, y=5)
         pass
 
     def afficher_troncons(self):
@@ -89,4 +139,31 @@ class Vue:
         pass
 
     def construire_tour_poison(self):
+        pass
+
+    def upgrade(self):
+        pass
+
+    def split_string_amelioration(self, str_am):
+        cout = str_am.split(";", 2)[0]
+        desc = str_am.split(";", 2)[1]
+        tour = str_am.split(";", 2)[2]
+        print(cout)
+        print(desc)
+        print(tour)
+
+        return [cout, desc, tour]
+        pass
+
+    def test_toggle_construction(self):
+        print("showing construction")
+        self.toggle_interface("f_amelioration", "f_construction")
+
+    def test_toggle_amelioration(self):
+        print("showing amelioration")
+        self.toggle_interface("f_construction", "f_amelioration")
+
+    def toggle_interface(self, remove, show):
+        self.dict_interfaces[remove].place_forget()
+        self.dict_interfaces[show].place(x=6 * self.modele.unite_base, y=5)
         pass
